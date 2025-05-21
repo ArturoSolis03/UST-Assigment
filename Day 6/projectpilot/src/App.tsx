16fe6b8d1fcc647ea65f52125d8dbe3d36154794
unknown <232940@ust.com>
2025-05-21 16:23:51 +0000

    LAb 20

---
+++ Day 6/projectpilot/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProjectsPage from './ProjectsPage';
+import { MOCK_PROJECTS } from './components/MockProjects';
+import { BrowserRouter, Routes, Route, NavLink} from 'react-router';
+import HomePage from './home/HomePage';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/"  className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+         <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App
