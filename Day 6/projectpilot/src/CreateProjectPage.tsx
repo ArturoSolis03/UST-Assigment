0a90c30d0f04d707127aaac6848e9dba1ffa3e7e
unknown <232940@ust.com>
2025-05-26 19:04:04 +0000

    Add validatios for empty spaces

--- Day 6/projectpilot/src/CreateProjectPage.tsx
+++ Day 6/projectpilot/src/CreateProjectPage.tsx
@@ -1,9 +1,16 @@
 
 import { useForm } from 'react-hook-form';
 import { projectAPI } from '../api/projectAPI';
+
+type FormValues ={
+      name: string;
+      description: string;
+      budget: number;
+      isActive : boolean;
+}
  
 export default function CreateProjectPage() {
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const { register, handleSubmit, reset, formState: { errors } } = useForm<FormValues>();
  
   const onSubmit = async (data: any) => {
     await projectAPI.create(data);
@@ -15,22 +22,28 @@ export default function CreateProjectPage() {
 <form onSubmit={handleSubmit(onSubmit)}>
 <h2>Create Project</h2>
  
-      <div>
+<div>
 <label>Project name:</label>
-<input {...register('name', { required: true, maxLength: 100 })} />
-        {errors.name && <span> Project name </span>}
+<input {...register('name', { required: 'Name is required', 
+      maxLength: 100,
+      validate: (value) =>
+      value.trim().length > 0 || 'Name must not be just spaces' })} />
+        {errors.name && <p style={{color: 'red'}}>{errors.name.message}</p>}
 </div>
  
       <div>
 <label>Project description:</label>
-<textarea {...register('description', { required: true, maxLength: 2000 })} />
-        {errors.description && <span> Project description </span>}
+<input {...register('description', { required: true, 
+maxLength: 2000,
+validate: (value) => 
+value.trim().length > 0 || 'Description must not be just spaces' })} />
+        {errors.description && <p style={{color: 'red'}}>{errors.description.message}</p>}
 </div>
  
       <div>
 <label>Project budget:</label>
 <input type="number" {...register('budget', { required: true, min: 1 })} />
-        {errors.budget && <span> Project budget </span>}
+        {errors.budget && <span> Project budget required </span>}
 </div>
  
       <div>
