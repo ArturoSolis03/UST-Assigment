327acb8f48796b5c4bb5fc8b2c4206cdd35b7e41
unknown <232940@ust.com>
2025-05-27 18:20:18 +0000

    Conection with mongodb

--- Day 6/projectpilot/api/projectAPI.ts
+++ Day 6/projectpilot/api/projectAPI.ts
@@ -1,7 +1,11 @@
 
 import axios from 'axios';
  
+const API_URL = 'http://localhost:4000/projects';
+
 export const projectAPI = {
-  create: (project: any) => axios.post('http://localhost:4000/projects', project),
-  list: () => axios.get('http://localhost:4000/projects'),
+  create: async (project: any) => {
+    const response = await axios.post(API_URL, project);
+    return response.data;
+  },
 };
