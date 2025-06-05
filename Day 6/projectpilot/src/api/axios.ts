1c1d2fd126168c7f15110650c5a32e071ef9c54d
unknown <232940@ust.com>
2025-06-05 20:52:26 +0000

    Implement refresh token

--- Day 6/projectpilot/src/api/axios.ts
+++ Day 6/projectpilot/src/api/axios.ts
@@ -1,35 +1,88 @@
-
 import axios from 'axios';
  
 const instance = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: 'http://localhost:3000',
+  withCredentials: true, 
 });
+ 
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-  }else{
-    console.warn('Token not exists');
   }
   return config;
 });
+ 
+
+let isRefreshing = false;
+let failedQueue: any[] = [];
+ 
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+ 
 
 instance.interceptors.response.use(
   response => response,
-  error => {
-    const status = error.response?.status;
+  async error => {
+    const originalRequest = error.config;
  
-    if (status === 401 || status === 403) {
-      console.warn('Token expired or unauthorized. Logging out...');
+    
+    if ((error.response?.status === 401 || error.response?.status === 403) && !originalRequest._retry) {
+      originalRequest._retry = true;
  
-      
-      localStorage.removeItem('accessToken');
+      if (isRefreshing) {
+        
+        return new Promise((resolve, reject) => {
+          failedQueue.push({
+            resolve: (token: string) => {
+              originalRequest.headers.Authorization = `Bearer ${token}`;
+              resolve(instance(originalRequest));
+            },
+            reject: (err: any) => reject(err),
+          });
+        });
+      }
  
-      
-      window.location.href = '/';
+      isRefreshing = true;
  
-      
+      try {
+        const refreshToken = localStorage.getItem('refreshToken');
+        const userId = localStorage.getItem('userId');
+ 
+        const res = await axios.post('http://localhost:3000/auth/refresh', {
+          userId,
+          refreshToken,
+        });
+ 
+        const { accessToken, refreshToken: newRefreshToken } = res.data;
+ 
+        
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', newRefreshToken);
+ 
+        processQueue(null, accessToken);
+ 
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+        return instance(originalRequest);
+      } catch (err) {
+        processQueue(err, null);
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        localStorage.removeItem('userId');
+        window.location.href = '/';
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
     }
  
     return Promise.reject(error);
