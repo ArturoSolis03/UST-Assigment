f07b5a08707da677af3884c5e53ab9beb3aed00b
unknown <232940@ust.com>
2025-06-05 17:40:00 +0000

    Add cors policy for all

--- Nestjs-work/backend-service1/src/main.ts
+++ Nestjs-work/backend-service1/src/main.ts
@@ -2,25 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+ 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  //chnange this
+ 
   app.enableCors({
-  origin: true, 
-  credentials: true, 
-});
-
+    origin: (origin, callback) => {
+      
+      if (!origin || origin.startsWith('http://localhost')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
+ 
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+ 
   const config = new DocumentBuilder()
     .setTitle('Projects API')
     .setDescription('API for managing projects')
     .setVersion('1.0')
     .build();
-
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+ 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+ 
   await app.listen(3000);
 }
 bootstrap();
