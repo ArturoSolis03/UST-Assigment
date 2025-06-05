da81d4df796f94d0092638cb4134dc69f9a37f6e
unknown <232940@ust.com>
2025-06-05 16:24:24 +0000

    Add cors policy 2

--- Nestjs-work/backend-service1/src/main.ts
+++ Nestjs-work/backend-service1/src/main.ts
@@ -6,7 +6,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
 
-  app.enableCors({});
+  app.enableCors({
+  origin: 'http://localhost:52104', 
+  credentials: true, 
+});
 
   const config = new DocumentBuilder()
     .setTitle('Projects API')
