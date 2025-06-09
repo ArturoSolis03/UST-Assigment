18ad0126854760d52c9d60df111bf9a850f7b3e4
ArturoSolis03 <232940@ust.com>
2025-06-09 14:46:15 -0600

    Conection with docker

--- Nestjs-work/backend-service1/src/main.ts
+++ Nestjs-work/backend-service1/src/main.ts
@@ -2,13 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
- 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
+
   app.enableCors({
     origin: (origin, callback) => {
-      
       if (!origin || origin.startsWith('http://localhost')) {
         callback(null, true);
       } else {
@@ -17,18 +16,18 @@ async function bootstrap() {
     },
     credentials: true,
   });
- 
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
- 
+
   const config = new DocumentBuilder()
     .setTitle('Projects API')
     .setDescription('API for managing projects')
     .setVersion('1.0')
     .build();
- 
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
- 
+
   await app.listen(3000);
 }
 bootstrap();
